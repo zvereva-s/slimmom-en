91fec91361a87543ae9e3147c79f7195202397dc
Nefalit <nefalit7@gmail.com>
2022-08-01 20:58:00 +0300

    add modal start

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,13 @@
+import Home from 'modules/Home';
+
 function MainPage() {
-    return (
-        <main>
-            <div className="container">
-                <h1>Main Page</h1>
-                </div>
-        </main>
-    )
- };
+  return (
+    <main>
+      <div className="container">
+        <h1>Main Page</h1>
+        <Home />
+      </div>
+    </main>
+  );
+}
 export default MainPage;

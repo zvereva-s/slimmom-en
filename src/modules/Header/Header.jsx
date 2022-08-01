59338d18d8383181a65fb2491837541c3c18e9d1
Pavlo <75275133+MrPaul007@users.noreply.github.com>
2022-08-01 21:32:47 +0300

    added header

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,11 +1,18 @@
 import HeaderMenu from "./HeaderMenu";
+import Logo from "../../shared/components/Logo";
+
+import styles from "./header.module.css";
 
 function Header() { 
     return (
-        <header>
-            <nav>
-                <HeaderMenu />
-            </nav>
+        <header className={styles.header}>
+            <div className="container">          
+                <nav className={styles.row}>
+                    <Logo />
+                    <div className={styles.line}></div>
+                    <HeaderMenu />
+                </nav>
+            </div>
         </header>
     )
 };

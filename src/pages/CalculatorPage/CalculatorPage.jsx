53b80754b65462c4e0e7fa4728e4dcb28eaf0e0e
Denys Lazarenko <dentus25@gmail.com>
2022-08-02 00:20:29 +0300

    add_styles_btn

--- src/pages/CalculatorPage/CalculatorPage.jsx
+++ src/pages/CalculatorPage/CalculatorPage.jsx
@@ -1,11 +1,8 @@
-import CalculatorForm from "modules/Calculator/CalculatorForm";
-
 function CalculatorPage() {
     return (
         <main>
             <div className="container">
                 <h1>Calculate your daily calorie intake right now</h1>
-                <CalculatorForm />
             </div>
         </main>
     )

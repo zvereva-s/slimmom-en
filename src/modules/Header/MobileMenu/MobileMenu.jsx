c1e14a16b08ab6d9cfd6df9ffd9aca36179309f2
Pavlo <75275133+MrPaul007@users.noreply.github.com>
2022-08-02 23:03:52 +0300

    added styles

--- src/modules/Header/MobileMenu/MobileMenu.jsx
+++ src/modules/Header/MobileMenu/MobileMenu.jsx
@@ -1,33 +1,42 @@
+import { createPortal } from "react-dom";
 import { NavLink } from 'react-router-dom';
 
 import styles from "./mobile-menu.module.css"
+import "./mobile-menu.css"
 
 import { items } from '../HeaderMenu/items';
 
 function getClassName({ isActive }) {
-    const style = isActive ? styles.isActive : styles.link;
-    return style;
-  }
+  const style = isActive ? styles.isActive : styles.link;
+  return style;
+}
+
+const modalRoot = document.getElementById("modal");
 
 function MobileMenu({onClick}) {
 
-    const selectItems =items.filter(item => item.private);
+  const selectItems =items.filter(item => item.private);
   
-    const elements = selectItems.map(({id, link, title}) => 
-      <li key={id} className={styles.item}>
-        <NavLink className={getClassName} onClick={onClick} to={link}>
-          {title}
-        </NavLink>
-      </li>
-    );
+  const elements = selectItems.map(({id, link, title}) => 
+    <li key={id} className={styles.item}>
+      <NavLink className={getClassName} onClick={onClick} to={link}>
+        {title}
+      </NavLink>
+    </li>
+  );
 
-    return (
-        <div className={styles.mobileContainer} >
-            <ul className={styles.mobileMenu} > 
-               {elements}
-            </ul>
+  return (
+    createPortal(
+      (
+        <div id='mobile-menu' className='menu-box'>
+          <div className={styles.mobileContainer} >
+              <ul className={styles.mobileMenu} > 
+                 {elements}
+              </ul>
+          </div>
         </div>
-    )
+      ), modalRoot)      
+  )
 }
 
 export default MobileMenu;

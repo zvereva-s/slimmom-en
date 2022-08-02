52ab6f3f3242a9a873699483ad5e24780bde1e31
Denys Lazarenko <dentus25@gmail.com>
2022-08-02 13:22:28 +0300

    add_calendar_&_diaryProduct

--- src/modules/Diary/DiaryDateСalendar/DiaryDateСalendar.jsx
+++ src/modules/Diary/DiaryDateСalendar/DiaryDateСalendar.jsx
@@ -1,17 +1,23 @@
 import { useState } from 'react';
-import Calendar from 'react-calendar';
+// import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+
+import styles from './diary-date-calendar.module.css'
 
 function DiaryDateCalendar() { 
     const [value, onChange] = useState(new Date());
 
     console.log(value)
-    
 
     return (
-    <div>
-            <Calendar onChange={onChange} value={value} calendarType="ISO 8601" />
-    </div>
+        
+        <div>
+            <input className={styles.input} type="date" />
+            
+            {/* <Calendar onChange={onChange} value={value} calendarType="ISO 8601" /> */}
+            
+        </div>
+        
     )
 };
 export default DiaryDateCalendar;

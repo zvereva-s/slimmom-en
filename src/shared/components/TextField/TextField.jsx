8f2e58fe30ca14c64a8262bc7440ebed2ee4981f
Pavlo <75275133+MrPaul007@users.noreply.github.com>
2022-08-04 00:56:00 +0300

    added mobile menu for diary form and summary in calculator page

--- src/shared/components/TextField/TextField.jsx
+++ src/shared/components/TextField/TextField.jsx
@@ -3,13 +3,13 @@ import { nanoid } from "@reduxjs/toolkit";
 
 import styles from './text-field.module.css'
 
-function TextField({ label, name, type, value, onChange, placeholder, required }) { 
+function TextField({ label, name, type, value, onChange, placeholder, required, style }) { 
     const id = useMemo(() => nanoid(), []);
-
+    const fieldStyles = style ? styles.inputGrams : styles.input;
     return (
         <div>
             {label && <label htmlFor={id} >{label}</label>}
-            <input className={styles.input} id={id} name={name} type={type} value={value} onChange={onChange} placeholder={placeholder} required={required} />
+            <input className={fieldStyles} id={id} name={name} type={type} value={value} onChange={onChange} placeholder={placeholder} required={required} />
         </div>
     )
 };

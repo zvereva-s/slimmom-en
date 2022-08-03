ab634cd34d4ef8410ba3f3b1ad0fd9ef55436690
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-08-03 21:31:04 +0300

    Update DiaryAddProductForm.jsx

--- src/modules/Diary/DiaryAddProductForm/DiaryAddProductForm.jsx
+++ src/modules/Diary/DiaryAddProductForm/DiaryAddProductForm.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
 import { DebounceInput } from 'react-debounce-input';
-import Select from 'react-select';
 
 
 import useForm from '../../../shared/hooks/useForm';

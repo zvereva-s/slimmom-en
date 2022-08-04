c904c26afb84ace802289dd80fdad0296a626eff
Pavlo <75275133+MrPaul007@users.noreply.github.com>
2022-08-04 18:28:06 +0300

    added data to summary

--- src/modules/Summary/Summary.jsx
+++ src/modules/Summary/Summary.jsx
@@ -1,18 +1,40 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import { getDiaryState } from 'redux/diary/diary-selectors';
+import { getDailyInfo } from 'redux/daily/daily-selectors';
 
 import SummaryList from "./SummaryList";
 
 import styles from './summary.module.css';
 
 function Summary() { 
+    const { dateInfo, product } = useSelector(getDiaryState);
+    const { notAllowedProducts } = useSelector(getDailyInfo);
+    
+    const getSummaryItems = () => {
+        let obj = {
+            kcalLeft: 0,
+            kcalConsumed: 0,
+            dailyRate: 0,
+            percentsOfDailyRate: 0
+        }
 
-    const { dateInfo } = useSelector(getDiaryState);
+        if (product.daySummary) {
+            return product.daySummary
+        } else if (product.newSummary) {
+            return product.newSummary
+        } 
+        
+        return obj;
+    }
 
+    const summaryItems = product.daySummary;
+    console.log(typeof(summaryItems));
 
-    const items = dateInfo.response ? dateInfo.response : [];
+    const items = getSummaryItems();
 
-    const elements = [].map(el => <li></li>);
+    const elements = notAllowedProducts.length ? notAllowedProducts.map(el => <span>{el}{", "}</span>) : [];
 
     return (
         <div className={styles.wrapper}>
@@ -22,7 +44,7 @@ function Summary() {
             </div>
             <div>
                 <h3 className={styles.title}>Food not recommended</h3>
-                <ul>{elements}</ul>
+                <div className={styles.footerFood} >{elements}</div>
             </div>
         </div>
     )

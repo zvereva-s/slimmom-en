98b06aba75ea71edc4dfa240447d825c6da6b286
Nefalit <nefalit7@gmail.com>
2022-08-06 01:31:18 +0300

    fix scroll product list diary & add token to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import useAuth from 'shared/hooks/useAuth';
 
 import { getCurrentRequest } from 'redux/auth/auth-operations';
+import { getToken } from 'redux/auth/auth-selectors';
 
-import BackGround from "./modules/BackGround";
+import BackGround from './modules/BackGround';
 import UserRoutes from './UserRoutes';
 import Header from './modules/Header';
 import Notification from 'shared/components/Notification';
 
-
-
-
 function App() {
   const dispatch = useDispatch();
   const isLogin = useAuth();
-  
+  const token = useSelector(getToken);
+
   useEffect(() => {
-    if (isLogin){
-      dispatch(getCurrentRequest())
+    if (isLogin || token) {
+      dispatch(getCurrentRequest());
     }
   }, [dispatch]);
 
@@ -30,5 +29,5 @@ function App() {
       <Notification />
     </>
   );
-};
+}
 export default App;
